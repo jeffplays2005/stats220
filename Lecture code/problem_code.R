6c6b8c3bdee925a6f6d725f9c92587af70b993e3
Jeffery <61447509+jeffplays2005@users.noreply.github.com>
2025-03-24 11:34:47 +1300

    Add demo code

--- Lecture code/problem_code.R
+++ Lecture code/problem_code.R
@@ -1,8 +1,8 @@
 library(magick)
 
 my_meme <- image_read("https://cataas.com/cat") %>%
-  image_annotate(text = "What you looking at?", font = "Impact", size = 100)
+  image_annotate(text = "What yo looking at?", font = "Impact", size = 100) %>%
+  image_scale(200)
 
 image_write(my_meme, "my_meme.png")
 my_meme
-
